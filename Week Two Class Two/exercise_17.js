0efd4825e318800d89eba8b36faad8f71a484a4d
fender-kid <jtt.sides@gmail.com>
2023-09-04 19:52:46 -0500

    this code works for single words but not phrases

---
+++ Week Two Class Two/exercise_17.js
@@ -0,0 +1,23 @@
+function reverse_word(word) {
+    return word.split('').reverse().join('');
+}
+
+function isPalindrome(word) {
+    if (word == reverse_word(word)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// tests
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
+console.log(isPalindrome("Was it a car or a cat I saw?")); // true
+console.log(isPalindrome("hello")); // false
+console.log(isPalindrome("Palindrome")); // false
+console.log(isPalindrome("12321")); // true
+console.log(isPalindrome("No lemon, no melon")); // true
+console.log(isPalindrome("Madam, in Eden, I'm Adam")); // true
+console.log(isPalindrome("")); // true (empty string is considered a palindrome)
+console.log(isPalindrome("abcdeedcba")); // true (long palindrome)
